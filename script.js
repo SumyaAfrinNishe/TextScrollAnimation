30be15c1250f757371ea4cf9a308d286578de835
SumyaAfrinNishe <afrinnishe99@gmail.com>
2024-07-07 04:05:00 +0600

    Swith JS code

---
+++ script.js
@@ -0,0 +1,31 @@
+let textElement = document.querySelector('.text');
+        let textContent = textElement.textContent;
+        textElement.innerHTML = '';
+
+        let spans =[];
+        for(let char of textContent)
+    {
+        let span = document.createElement('span');
+        span.textContent =char;
+        textElement.appendChild(span);
+        spans.push(span);
+    }
+
+    // add scroll event listener
+    window.addEventListener('scroll' , () => {
+        let scrollDistance = window.scrollY;
+        spans.forEach((span, index) => {
+            if(scrollDistance >= (index + 1) * 50)
+        {
+            span.style.transform = `translate(${index * 20}px,0)`;
+            span.classList.add('active');
+        }
+        else{
+           span.style.transform = `translate(${Math.random() * 100 - 50}vw, ${Math.random() * 100 - 50}vh)`;
+           span.classList.remove('active');
+        }
+        })
+    })
+
+    //Trigger initial random position
+    window.dispatchEvent(new Event('scroll'));
